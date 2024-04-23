f06040c00ceebc1bcc16d5d480de5d3363aad20b
LeeYuCHhul <yoochul89@naver.com>
2024-04-23 22:31:04 +0900

    0423_약관 및 개인정보 방침 페이지 로직 구성

--- my-app/src/screen/term/TermScreen.js
+++ my-app/src/screen/term/TermScreen.js
@@ -1,19 +1,43 @@
 import React, { useState, useEffect } from 'react';
-import termsData from '../../data/terms.json'; // 경로는 JSON 파일의 실제 위치에 따라 달라질 수 있습니다.
+import { useLocation } from 'react-router-dom';
+import termsData from '../../data/terms.json'; 
+import privateData from '../../data/PrivateProcess.json'; 
+import styles from './TermScreenStyles.module.css';
 
 function TermPage() {
     const [terms, setTerms] = useState([]);
+    const [PrivateProcess, setPrivate] = useState([]);
+    const location = useLocation();  // 현재 위치 정보를 가져옵니다.
 
     useEffect(() => {
         setTerms(termsData);
-    }, []);
+        setPrivate(privateData);
+
+        // 페이지 로드 후 location.hash에 따라 스크롤
+        if (location.hash) {
+            const id = location.hash.replace("#", "");
+            const element = document.getElementById(id);
+            if (element) {
+                element.scrollIntoView();
+            }
+        }
+    }, [location]); // location 변경 감지
 
     return (
         <div>
-            <h1>이용약관</h1>
+            <br /><br />
+            <h1 className={styles.TermPageH1}>이용약관</h1>
             {terms.map((term, index) => (
-                <div key={index}>
-                    <h2>{term.소제목}</h2>
+                <div key={index} className={styles.content}>
+                    <h2 className={styles.TermPageH2}>{term.소제목}</h2>
+                    <p>{term.내용}</p>
+                </div>
+            ))}
+            <br /><br /><br /><br />
+            <h1 id="privacyPolicy" className={styles.TermPageH1}>개인정보 처리방침</h1>
+            {PrivateProcess.map((term, index) => (
+                <div key={index} className={styles.content}>
+                    <h2 className={styles.TermPageH2}>{term.소제목}</h2>
                     <p>{term.내용}</p>
                 </div>
             ))}

a1c85400cc5fc2b0f0d49e63e8ac49f033e1e106
LeeYuCHhul <yoochul89@naver.com>
2024-05-14 22:08:50 +0900

    0514_홈페이지 URL추가

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import QuestionPage from './QuestionPage';
 import TermPage from './screen/term/TermScreen';
-
+import HomePage from './screen/home/HomeScreen';
+ 
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/home" element={<HomePage />} />
         <Route path="/api/v1/web/question/:announcementId" element={<QuestionPage />} />
         <Route path="/term" element={<TermPage />} />
       </Routes>
---
+++ my-app/src/screen/home/HomeScreen.js
@@ -0,0 +1,13 @@
+// src/screen/home/HomeScreen.js
+
+import React from 'react';
+
+function HomeScreen() {
+    return (
+        <div>
+            <h1>스타팅블록</h1>
+        </div>
+    );
+}
+
+export default HomeScreen;
